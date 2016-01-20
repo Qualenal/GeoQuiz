4b33e87dfac38ec23421c55f8cee6bb858bbf9bd
Sam Kennedy <skennedy@mymail.mines.edu>
2016-01-20 15:49:50 -0700

    Finished BNR chapter 2 challenges

--- app/src/main/java/com/example/sam/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/sam/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,7 +12,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -75,12 +77,21 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = mCurrentIndex == 0 ? (mQuestionBank.length - 1) : mCurrentIndex - 1;
                 updateQuestion();
             }
         });
