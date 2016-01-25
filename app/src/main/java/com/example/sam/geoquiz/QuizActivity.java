61c0a4e775f533772818f4b6b7dede7dad1bf906
Sam Kennedy <skennedy@mymail.mines.edu>
2016-01-24 23:46:37 -0700

    Finished Assignment 1

--- app/src/main/java/com/example/sam/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/sam/geoquiz/QuizActivity.java
@@ -4,30 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private TextView mQuestionTextView;
-    Button mMcButtonOne = (Button) findViewById(R.id.mc_button_one);
-    Button mMcButtonTwo = (Button) findViewById(R.id.mc_button_two);
-    Button mMcButtonThree = (Button) findViewById(R.id.mc_button_three);
-    Button mMcButtonFour = (Button) findViewById(R.id.mc_button_four);
+
 
     private Question[] mQuestionBank = new Question[] {
             new TrueFalseQuestion(R.string.question_oceans, true),
-            new TrueFalseQuestion(R.string.question_mideast, false),
-            new TrueFalseQuestion(R.string.question_africa, false),
-            new TrueFalseQuestion(R.string.question_americas, true),
-            new TrueFalseQuestion(R.string.question_asia, true),
             new MultipleChoiceQuestion(R.string.question_beer, new int[]{R.string.answer_beer_one,
-                    R.string.answer_beer_two,R.string.answer_beer_three,R.string.answer_beer_four},1)
+                    R.string.answer_beer_two,R.string.answer_beer_three,R.string.answer_beer_four},1),
+            new FillInBlankQuestion(R.string.question_capital,"Denver")
     };
-
     private int mCurrentIndex = 0;
+
+    private void setUpQuestions(){
+        ((MultipleChoiceQuestion) mQuestionBank[1]).setAnswers(new int[]{R.string.answer_beer_one,
+                R.string.answer_beer_two, R.string.answer_beer_three, R.string.answer_beer_four});
+    }
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
@@ -37,23 +37,84 @@ public class QuizActivity extends AppCompatActivity {
             findViewById(R.id.mc_question_view).setVisibility(View.GONE);
             findViewById(R.id.fill_in_question_layout).setVisibility(View.GONE);
         } else if (mQuestionBank[mCurrentIndex] instanceof FillInBlankQuestion) {
+
             findViewById(R.id.true_false_view).setVisibility(View.GONE);
             findViewById(R.id.mc_question_view).setVisibility(View.GONE);
             findViewById(R.id.fill_in_question_layout).setVisibility(View.VISIBLE);
+
+            Button mSubmitButton = (Button) findViewById(R.id.submit_button);
+            final EditText mTextField = (EditText) findViewById(R.id.submit_text_area);
+            mSubmitButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    FillInBlankQuestion q = (FillInBlankQuestion) mQuestionBank[mCurrentIndex];
+                    String answer = mTextField.getText().toString().trim();
+                    q.setUserAnswer(answer);
+                    checkAnswer();
+                }
+            });
+
         } else if (mQuestionBank[mCurrentIndex] instanceof MultipleChoiceQuestion) {
+            MultipleChoiceQuestion q = (MultipleChoiceQuestion) mQuestionBank[mCurrentIndex];
             findViewById(R.id.true_false_view).setVisibility(View.GONE);
-            findViewById(R.id.mc_question_view).setVisibility(View.GONE);
-            findViewById(R.id.fill_in_question_layout).setVisibility(View.VISIBLE);
+            findViewById(R.id.mc_question_view).setVisibility(View.VISIBLE);
+            findViewById(R.id.fill_in_question_layout).setVisibility(View.GONE);
+
+            Button mMcButtonOne = (Button) findViewById(R.id.mc_button_one);
+            Button mMcButtonTwo = (Button) findViewById(R.id.mc_button_two);
+            Button mMcButtonThree = (Button) findViewById(R.id.mc_button_three);
+            Button mMcButtonFour = (Button) findViewById(R.id.mc_button_four);
+            mMcButtonOne.setText(q.getAnswers()[0]);
+            mMcButtonTwo.setText(q.getAnswers()[1]);
+            mMcButtonThree.setText(q.getAnswers()[2]);
+            mMcButtonFour.setText(q.getAnswers()[3]);
+            mMcButtonOne.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    ((MultipleChoiceQuestion) mQuestionBank[mCurrentIndex]).setUserAnswer(1);
+                    checkAnswer();
+                }
+            });
+
+            mMcButtonTwo.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    ((MultipleChoiceQuestion) mQuestionBank[mCurrentIndex]).setUserAnswer(2);
+                    checkAnswer();
+                }
+            });
+
+            mMcButtonThree.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    ((MultipleChoiceQuestion) mQuestionBank[mCurrentIndex]).setUserAnswer(3);
+                    checkAnswer();
+                }
+            });
+
+            mMcButtonFour.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    ((MultipleChoiceQuestion) mQuestionBank[mCurrentIndex]).setUserAnswer(4);
+                    checkAnswer();
+                }
+            });
         }
     }
 
     private void checkAnswer() {
         boolean answerIsCorrect =  mQuestionBank[mCurrentIndex].isAnswerTrue();
-
+        ProgressBar mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
         int messageResId;
 
         if(answerIsCorrect){
             messageResId = R.string.correct_toast;
+            mProgressBar.incrementProgressBy(1);
         } else {
             messageResId = R.string.incorrect_toast;
         }
@@ -95,41 +156,6 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mMcButtonOne.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                ((MultipleChoiceQuestion) mQuestionBank[mCurrentIndex]).setUserAnswer(1);
-                checkAnswer();
-            }
-        });
-
-        mMcButtonTwo.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                ((MultipleChoiceQuestion) mQuestionBank[mCurrentIndex]).setUserAnswer(2);
-                checkAnswer();
-            }
-        });
-
-        mMcButtonThree.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                ((MultipleChoiceQuestion) mQuestionBank[mCurrentIndex]).setUserAnswer(3);
-                checkAnswer();
-            }
-        });
-
-        mMcButtonFour.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                ((MultipleChoiceQuestion) mQuestionBank[mCurrentIndex]).setUserAnswer(4);
-                checkAnswer();
-            }
-        });
 
         ImageButton nextButton = (ImageButton) findViewById(R.id.next_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
@@ -150,4 +176,5 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
     }
+
 }
